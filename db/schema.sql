71776f97d514254670605eab417de056b37e8e00
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-10-10 11:24:10 -0600

    added tables

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER FOREIGN KEY () REFERENCES (), 
+    manager_id INTEGER FOREIGN KEY () REFERENCES ()
+);
+
+CREATE TABLE employee_role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL (),
+    department_id INTEGER FOREIGN KEY () REFERENCES ()
+);
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    employee_name VARCHAR(30)
+);
