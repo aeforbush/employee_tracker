465dd7b3e6fe35fadd529c3a0119b5f8bac132ad
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-10-15 16:23:07 -0600

    code review

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,29 @@
-CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER FOREIGN KEY () REFERENCES (), 
-    manager_id INTEGER FOREIGN KEY () REFERENCES ()
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS employee_role;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    employee_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee_role (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL (),
-    department_id INTEGER FOREIGN KEY () REFERENCES ()
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-CREATE TABLE department (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    employee_name VARCHAR(30)
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    PRIMARY Key (id),
+    FOREIGN KEY (role_id) REFERENCES employee_role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE   
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,29 @@
-INSERT INTO employee ()
+INTEGER INTO department (id, name) 
+VALUES
+(1, 'IT'),
+(2, 'Marketing'),
+(3, 'Accounting'),
+(4, 'Operations');
+
+INSERT INTO employee_role (id, title, salary, department_id)
+VALUES
+(1, 'Full Stack Developer', 80000, 1),
+(2, 'Database Architect', 125000, 1),
+(3, 'Software Engineer', 90000, 1),
+(4, 'Sales Lead', 85000, 2),
+(5, 'Marketing Analyst', 80000, 2),
+(6, 'Accountant', 75000, 3),
+(7, 'Project Manager', 100000, 4),
+(8, 'Operations Director', 130000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+(1, 'Ada', 'Lovelace', 1, 1),
+(2, 'Ida', 'Tarbell', 2, 1),
+(3, 'Elanor', 'Roosevelt', 4, 1),
+(4, 'Jane', 'Goodall', 2, 0),
+(5, 'Georgia', 'OKeefe', 2, 0),
+(6, 'Rachel', 'Carson', 4, 1),
+(7, 'Marie', 'Curie', 3, 0),
+(8, 'Elizabeth', 'Cady Stanton', 1, 0),
+(9, 'Grace', 'Hopper', 1, 1);
