bd60d4802f087aff83981d236943cc3b34a0bbc9
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-10-16 13:20:51 -0600

    updated table

--- db/schema.sql
+++ db/schema.sql
@@ -3,27 +3,24 @@ DROP TABLE IF EXISTS employee_role;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
-    id INTEGER AUTO_INCREMENT NOT NULL,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE employee_role (
-    id INTEGER AUTO_INCREMENT NOT NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    FOREIGN KEY (department_id) REFERENCES department(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT NOT NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER,
-    PRIMARY Key (id),
-    CONSTRAINT fk_employee_role FOREIGN KEY (role_id) REFERENCES employee_role(id) ON DELETE CASCADE,
-    CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE   
+    CONSTRAINT fk_employee_role FOREIGN KEY (role_id) REFERENCES employee_role(id) ON UPDATE CASCADE,
+    CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee_role(id) ON UPDATE CASCADE   
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO department (id, name) 
+INSERT INTO department (id, dept_name) 
 VALUES
 (1, 'IT'),
 (2, 'Marketing'),
